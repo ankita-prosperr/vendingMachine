586a510e62933a3f6cbdbfe38233dad87fa2909f
Kumar Dev <devkumar2300.dk@gmail.com>
2026-01-13 14:54:42 +0530

    Created UserController, User, UserRepository, UserService

---
+++ src/main/java/org/example/vendingmachine/controller/UserController.java
@@ -0,0 +1,52 @@
+package org.example.vendingmachine.controller;
+
+import org.example.vendingmachine.entity.User;
+import org.example.vendingmachine.service.UserService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    // CREATE
+    @PostMapping("/createUser")
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+
+    // READ by ID
+    @GetMapping("/getUser/{id}")
+    public User getUserById(@PathVariable Long id) {
+        return userService.getUserById(id);
+    }
+
+    // READ all
+    @GetMapping("getAllUsers")
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    // UPDATE
+    @PutMapping("/updateUser/{id}")
+    public User updateUser(
+            @PathVariable Long id,
+            @RequestBody User user
+    ) {
+        return userService.updateUser(id, user);
+    }
+
+    // DELETE
+    @DeleteMapping("/deleteUser/{id}")
+    public String deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+        return "User deleted successfully";
+    }
+}
---
+++ src/main/java/org/example/vendingmachine/service/UserService.java
@@ -0,0 +1,49 @@
+package org.example.vendingmachine.service;
+
+import org.example.vendingmachine.entity.User;
+import org.example.vendingmachine.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    // CREATE
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    // READ by ID
+    public User getUserById(Long id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+    }
+
+    // READ all
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    // UPDATE
+    public User updateUser(Long id, User user) {
+        User existingUser = getUserById(id);
+
+        existingUser.setName(user.getName());
+        existingUser.setEmail(user.getEmail());
+        existingUser.setPassword(user.getPassword());
+
+        return userRepository.save(existingUser);
+    }
+
+    // DELETE
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
+}
